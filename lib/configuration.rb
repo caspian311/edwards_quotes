1b8d650092a3323ad95bcf2d5fdda7b8a2c1cc84
Matt Todd <matt.c.todd@gmail.com>
2012-12-08 11:19:53 -0600

    oops

--- lib/configuration.rb
+++ lib/configuration.rb
@@ -1,12 +1,5 @@
 require "twitter"
 
-Twitter.configure do |conf|
-   conf.consumer_key = _read_file "conf/consumer_key.txt"
-   conf.consumer_secret = _read_file "conf/consumer_secret.txt"
-   conf.oauth_token = _read_file "conf/oauth_token"
-   conf.oauth_token_secret = _read_file "conf/oauth_token_secret"
-end
-
 def _read_file(filename)
    contents = ""
    File.open(filename, "r") do |file|
@@ -14,3 +7,11 @@ def _read_file(filename)
    end
    return contents
 end
+
+Twitter.configure do |conf|
+   conf.consumer_key = _read_file "conf/consumer_key.txt"
+   conf.consumer_secret = _read_file "conf/consumer_secret.txt"
+   conf.oauth_token = _read_file "conf/oauth_token.txt"
+   conf.oauth_token_secret = _read_file "conf/oauth_token_secret.txt"
+end
+
