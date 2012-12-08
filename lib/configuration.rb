a20b8f28a7ca57648fab759da7f4e2ea00063c6b
Matt Todd <matt.c.todd@gmail.com>
2012-12-08 11:17:57 -0600

    pulled out twitter authentications tokens to separate files to be read in so as not to put them into git history

--- lib/configuration.rb
+++ lib/configuration.rb
@@ -1,5 +1,16 @@
 require "twitter"
 
 Twitter.configure do |conf|
-#CENSORED!
+   conf.consumer_key = _read_file "conf/consumer_key.txt"
+   conf.consumer_secret = _read_file "conf/consumer_secret.txt"
+   conf.oauth_token = _read_file "conf/oauth_token"
+   conf.oauth_token_secret = _read_file "conf/oauth_token_secret"
+end
+
+def _read_file(filename)
+   contents = ""
+   File.open(filename, "r") do |file|
+      contents = file.read
+   end
+   return contents
 end
