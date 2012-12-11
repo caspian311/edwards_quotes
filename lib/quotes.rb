7e6daf52d15e4b21cf096134594e826f4b45707f
Matt Todd <matt.c.todd@gmail.com>
2012-12-10 20:05:51 -0600

    changed the quotes dao to use a file as the backend

--- lib/quotes.rb
+++ lib/quotes.rb
@@ -1,6 +1,22 @@
 
 class Quotes
+   def self.set_file(filename)
+      @@filename = filename
+   end
+
    def self.load_all
-      return ["quote 1", "quote 2", "quote 3"]
+      @@filename ||= "quotes.txt"
+
+      quotes = []
+      File.open(@@filename, "r") do |f|
+         f.readlines.each do |line|
+            line.strip!
+            if line != ""
+               quotes << line
+            end
+         end
+      end
+
+      return quotes
    end
 end
--- spec/quotes_spec.rb
+++ spec/quotes_spec.rb
@@ -2,8 +2,28 @@ require "spec_helper"
 
 describe Quotes do
    describe "#load_all" do
-      it "return all quotes" do
-         Quotes.load_all.count.should be > 1
+      before(:all) do
+         File.open("/tmp/quotes.txt", "w") do |f|
+            f.write """
+this is the first quote
+this is the second quote
+this is the third quote
+            """
+         end
+      end
+
+      after(:all) do
+         File.delete("/tmp/quotes.txt")
+      end
+
+      it "return all quotes from given file" do
+         Quotes.set_file "/tmp/quotes.txt"
+         all_quotes = Quotes.load_all
+
+         all_quotes.count.should be 3
+         all_quotes[0].should eq "this is the first quote"
+         all_quotes[1].should eq "this is the second quote"
+         all_quotes[2].should eq "this is the third quote"
       end
    end
 end
